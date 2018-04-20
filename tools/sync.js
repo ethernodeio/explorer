7a3fe3e358d261a2c7964bc447cdf67234a69b12
GeoSky Alert <mike@geoskygps.com>
2018-04-20 14:56:53 -0500

    Correct function variable token

--- tools/sync.js
+++ tools/sync.js
@@ -18,6 +18,7 @@ var Transaction     = mongoose.model( 'Transaction' );
 /**
   //Just listen for latest blocks and sync from the start of the app.
 **/
+var listenBlocks = function(config) {
     var newBlocks = web3.eth.filter("latest");
     newBlocks.watch(function (error,blockHashOrNumber) {
     if(error) {
