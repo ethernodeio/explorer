3a51bc09bd54be0f1c86cdef4751d61c65adb15a
GeoSky Alert <mike@geoskygps.com>
2018-04-21 17:49:13 -0500

    test

--- tools/sync.js
+++ tools/sync.js
@@ -190,4 +190,25 @@ if (!('gethPort' in config) || (typeof config.gethPort) !== 'number') {
 if (!('output' in config) || (typeof config.output) !== 'string') {
     config.output = '.'; // default this directory
 }
+//Look for config.json file if not
+try {
+    var configContents = fs.readFileSync('conf.json');
+    config = JSON.parse(configContents);
+    console.log('CONFIG FOUND: Node:'+config.nodeAddr+' | Port:'+config.gethPort);
+    // Sets address for RPC WEb3 to connect to, usually your node address defaults ot localhost
+    var web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.nodeAddr + ':' + config.gethPort.toString()));
+    // Starts full sync when set to true in config
+    if (config.syncAll === true){
+      syncChain(config,web3);
+    }
+}
+catch (error) {
+    if (error.code === 'ENOENT') {
+        console.log('No config file found. Using default configuration: Node:'+config.nodeAddr+' | Port:'+config.gethPort);
+    }
+    else {
+        throw error;
+        process.exit(1);
+    }
+}
 listenBlocks(config);
