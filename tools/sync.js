e7ac03f3fb10bcf59f855b65b037dbfac6cab05b
GeoSky Alert <mike@geoskygps.com>
2018-04-21 16:18:09 -0500

    test

--- tools/sync.js
+++ tools/sync.js
@@ -11,7 +11,6 @@ var BigNumber = require('bignumber.js');
 
 var fs = require('fs');
 var Web3 = require('web3');
-require('./config.js');
 
 var mongoose        = require( 'mongoose' );
 var Block           = mongoose.model( 'Block' );
