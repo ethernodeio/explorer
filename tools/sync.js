e1d528dda4016baa89345fc5de46164608124d64
GeoSky Alert <mike@geoskygps.com>
2018-04-21 20:04:55 -0500

    fix brackets

--- tools/sync.js
+++ tools/sync.js
@@ -74,18 +74,19 @@ var syncChain = function(config,web3,nextBlock){
 var writeBlockToDB = function(config, blockData) {
   return new Block(blockData).save( function( err, block, count ){
     if ( typeof err !== 'undefined' && err ) {
-        if (err.code == 11000) {
-            if(!('quiet' in config && config.quiet === true)) {
-              console.log('Skip: Duplicate key ' +  blockData.number.toString() + ': ' + err);
-            }
-        } else {
-          console.log('Error: Aborted due to error on ' + 'block number ' + blockData.number.toString() + ': ' +  err);
-          process.exit(9);
-       }
-    } else {
+      if (err.code == 11000) {
+        if(!('quiet' in config && config.quiet === true)) {
+          console.log('Skip: Duplicate key ' +  blockData.number.toString() + ': ' + err);
+        }
+      }else{
+        console.log('Error: Aborted due to error on ' + 'block number ' + blockData.number.toString() + ': ' +  err);
+        getOldestBlockDB();
+        //process.exit(9);
+      }else{
         if(!('quiet' in config && config.quiet === true)) {
           console.log('DB successfully written for block number ' + blockData.number.toString());
         }
+      }
     }
   });
 }
