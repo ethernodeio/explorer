3d4988cc41c84fef5fb7774487e820b9bc3608be
GeoSky Alert <mike@geoskygps.com>
2018-04-21 20:08:35 -0500

    fix

--- tools/sync.js
+++ tools/sync.js
@@ -75,17 +75,14 @@ var writeBlockToDB = function(config, blockData) {
   return new Block(blockData).save( function( err, block, count ){
     if ( typeof err !== 'undefined' && err ) {
       if (err.code == 11000) {
-        if(!('quiet' in config && config.quiet === true)) {
-          console.log('Skip: Duplicate key ' +  blockData.number.toString() + ': ' + err);
-        }
+        console.log('Skip: Duplicate key ' + blockData.number.toString() + ': ' +err);
       }else{
-        console.log('Error: Aborted due to error on ' + 'block number ' + blockData.number.toString() + ': ' +  err);
-        getOldestBlockDB();
-        //process.exit(9);
-      }else{
-        if(!('quiet' in config && config.quiet === true)) {
-          console.log('DB successfully written for block number ' + blockData.number.toString());
-        }
+        console.log('Error: Aborted due to error on ' + 'block number ' + blockData.number.toString() + ': ' + err);
+        process.exit(9);
+      }
+    }else{
+      if(!('quiet' in config && config.quiet === true)) {
+        console.log('DB successfully written for block number ' + blockData.number.toString() );
       }
     }
   });
