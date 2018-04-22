154fe383ba6ffedd7d60543f6f8b790e96a532f1
GeoSky Alert <mike@geoskygps.com>
2018-04-22 00:20:14 -0500

    test

--- tools/sync.js
+++ tools/sync.js
@@ -135,13 +135,12 @@ var checkBlockDBExistsThenWrite = function(config, blockData) {
   //Check oldest block in db and start sync from tehre
 **/
 var getOldestBlockDB = function() {
-  var blockFind = Block.find({}, "number").lean(true).sort('number').limit(2);
+  var blockFind = Block.find({}, "number").lean(true).sort('number').limit(1);
   blockFind.exec(function (err, docs) {
     if(docs.length < 1){
       console.log('nothing here starting from latest');
     }else{
       var nextBlock = (docs[0].number - 1);
-      console.log(docs);
       syncChain(config,web3,nextBlock);
     }
   });
